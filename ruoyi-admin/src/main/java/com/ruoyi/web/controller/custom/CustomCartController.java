cb405227cbcaebdfb20e073d3a34e8dfa2dce40d
HesionBlack <1026320617@qq.com>
2019-12-30 10:28:01 +0800

    数据库文件备份，药品管理页面新增按钮bug解决

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/custom/CustomCartController.java
@@ -0,0 +1,21 @@
+package com.ruoyi.web.controller.custom;/**
+ * ClassName: CustomCartController <br/>
+ * Description: <br/>
+ * date: 2019/12/30 10:15 上午<br/>
+ *
+ * @author Hesion<br />
+ * @version
+ * @since JDK 1.8
+ */
+
+/**
+ * @program: ruoyi
+ *
+ * @description:
+ *
+ * @author: hesion
+ *
+ * @create: 2019-12-30 10:15
+ **/
+public class CustomCartController {
+}
---
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysOrderView.java
@@ -0,0 +1,21 @@
+package com.ruoyi.system.domain;/**
+ * ClassName: SysOrderView <br/>
+ * Description: <br/>
+ * date: 2019/12/26 1:42 下午<br/>
+ *
+ * @author Hesion<br />
+ * @version
+ * @since JDK 1.8
+ */
+
+/**
+ * @program: ruoyi
+ *
+ * @description: 订单查看实体类
+ *
+ * @author: hesion
+ *
+ * @create: 2019-12-26 13:42
+ **/
+public class SysOrderView {
+}
